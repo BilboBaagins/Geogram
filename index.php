6520fde15c86309ca6aff24625836c61f6b9252d
BilboBaagins <archbold.billy@gmail.com>
2019-04-14 11:24:29 +0100

    Update index.php
    1. Swapped out the Google Geocode API for the DataScienceToolkit Geocode API 
    2. Swapped out the Instagram API for the Flickr API

--- index.php
+++ index.php
@@ -1,38 +1,47 @@
 <?php
 if (!empty($_GET['location'])) {
     /**
-     * Here we build the url we'll be using to access the google maps api
+     *    Notes:
+     *      1. YouTube tutorial by WebConcepts on how to mash the Google Geocode API together with the Instagram API.
+     *          https://www.youtube.com/watch?v=RTjd1nwvlj4
+     *      2. Both APIs have since changed their structures and the example no longer works - so I have adapted 
+     *          it to work using the DataScienceToolkit  API (in place of the Google Geocode API) and the Flickr API (in place of the Instagram API)
+     *      3. You need to set up a Flickr Account and get an API key - however, you can use the DataScienceToolkit API without the 
+     *          need to set up an account.
      */
-    $maps_url = 'https://' .
-        'maps.googleapis.com/' .
-        'maps/api/geocode/json' .
-        '?address=' . urlencode($_GET['location']);
+    $maps_url = 'http://www.datasciencetoolkit.org/maps/api/geocode/json?sensor=false&address=' . urlencode($_GET['location']);
     $maps_json = file_get_contents($maps_url);
     $maps_array = json_decode($maps_json, true);
-
     $lat = $maps_array['results'][0]['geometry']['location']['lat'];
     $lng = $maps_array['results'][0]['geometry']['location']['lng'];
-
     /**
-     * Time to make our Instagram api request. We'll build the url using the
-     * coordinate values returned by the google maps api
+     * Time to make our Flickr api request. We'll build the url using the
+     * coordinate values returned by the DataScienceToolkit geocode api
      */
-    $url = 'https://' .
-        'api.instagram.com/v1/media/search' .
-        '?lat=' . $lat .
-        '&lng=' . $lng .
-        '&client_id=CLIENT-ID'; //replace "CLIENT-ID"
-
-    $json = file_get_contents($url);
-    $array = json_decode($json, true);
+    $url = 'https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=_ENTER_YOUR_FLICKR_API_KEY_HERE_&lat=' . $lat . '&lon=' . $lng . '&format=json&nojsoncallback=1';
+        
+    //$json = file_get_contents($url);
+    //$array = json_decode($json, true);
+    
+    $data = json_decode(file_get_contents($url));
+    
+    //$img = 'http://farm'.$farm_id.'.staticflickr.com/'.$server_id.'/'.$photo_id.'_'.$secret_id.'_'.$size.'.'.'jpg';
+    
+    $photos = $data->photos->photo;
+    foreach($photos as $photo){
+        $img = 'http://farm'.$photo->farm.'.staticflickr.com/'.$photo->server.'/'.$photo->id.'_'.$photo->secret.'.jpg';
+        echo '<img src="'.$img.'">';
+        //print_r($photo);
+        //echo '<h1>'.$photo->title.'</h1>';
+    }
 }
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="utf-8"/>
-    <title>geogram</title>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+    <title>geoflickr</title>
     <script src="script.js"></script>
 </head>
 <body>
